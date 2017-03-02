bc2af2fd3bfb1659cc1810aead9c10b981867166
Yelitza Cho <yelitza.choque@gmail.com>
2017-03-02 18:31:05 -0500

    subiendo de nuevo por siacaso

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+  var nombre = document.getElementById('');
+  var apellido = document.getElementById('');
+  var email = document.getElementById('');
+  var password = document.getElementById('');
+
+
+
+
+  if((nombre.innerHTML).length<6){
+    alert('[ERROR] Ingrese la primera letra de tu nombre con mayúsculas');
+  }
+  else if () {
+    alert('[ERROR] Ingrese la primera letra de tu apellido con mayúculas');
+  }
+  else if () {
+    alert('[ERROR] Tu correo debería tener un formato similar: name@mundo.com');
+  }
+  else if (((document.getElementById('input-password')).innerHTML).length<6) {
+    alert('[ERROR] Tu password debe tener al menos 6 caracteres.');
+  }
+  else if () {
+    alert();
+  }
 }
